999d3c4b426e8082c0984f7bf1c6ea4f6234329e
makarenkoj <makarenkoj53@gmail.com>
2020-06-27 18:13:13 +0300

    my new logo

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -13,6 +13,8 @@ Spree.config do |config|
   # Example:
   # Uncomment to stop tracking inventory levels in the application
   # config.track_inventory_levels = false
+
+  config.logo = 'logo.jpg'
 end
 
 # Configure Spree Dependencies
